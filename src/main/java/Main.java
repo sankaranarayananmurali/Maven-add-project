d2af784db866600b2c369668f25e79ab15b07e58
sankaranarayananmurali <88310495+sankaranarayananmurali@users.noreply.github.com>
2025-12-17 16:26:30 +0530

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,14 @@
-import java.util.Scanner;
 package jar;
-import jar.App;
+
 public class Main {
-     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
 
-        System.out.print("Enter the first number: ");
-        int num1 = scanner.nextInt();
-
-        System.out.print("Enter the second number: ");
-        int num2 = scanner.nextInt();
-       
         App app = new App();
         int result = app.add(num1, num2);
-        scanner.close();
+
         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
     }
-
 }
+
